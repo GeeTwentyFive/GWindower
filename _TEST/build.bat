01cc6310c4498c02548177ac95965c5d6192ec94
Gee <alickoamarko@gmail.com>
2026-04-26 03:56:49 -0700

    Windows build scripts

--- _TEST/build.bat
+++ _TEST/build.bat
@@ -1,4 +1,1 @@
-cd ../build &&
-./build.bat &&
-cd ../_TEST &&
-c++ -O2 -march=x86-64-v2 -I ../libs/windows/glfw main.cpp ../build/libgwindower.a -o TEST.exe
+c++ -O2 -march=x86-64-v2 -I ../libs/windows/glfw main.cpp ../build/libgwindower.a -lgdi32 -lwinmm -o TEST.exe
--- build/build.bat
+++ build/build.bat
@@ -1,4 +1,4 @@
-c++ -O2 -march=x86-64-v2 -I ../libs/windows/glfw -c ../GWindower.cpp -o GWindower.o &&
-ar x ../libs/windows/glfw/libglfw3.a &&
-ar rcs libgwindower.a *.o
-rm *.o
+c++ -O2 -march=x86-64-v2 -I ../libs/windows/glfw -c ../GWindower.cpp -o GWindower.o
+ar x ../libs/windows/glfw/libglfw3.a
+ar rcs libgwindower.a *.o*
+del *.o*
